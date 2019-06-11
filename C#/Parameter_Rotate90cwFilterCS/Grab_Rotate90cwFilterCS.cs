f9d5b002d501ca9135818f05dfbafa5a89eb5a2d
setsu <setsu@sentech.co.jp>
2019-06-11 16:30:10 +0900

    -190611 Added sample for rotation filter (converter) using.

---
+++ C#/Parameter_Rotate90cwFilterCS/Grab_Rotate90cwFilterCS.cs
@@ -0,0 +1,101 @@
+﻿/*
+	Rotate90cwFilter: using Filter to rotate image clock wise 90 degree.
+*/
+
+#define ENABLED_ST_GUI
+
+using System;
+using Sentech.GenApiDotNET;
+using Sentech.StApiDotNET;
+
+namespace Grab
+{
+    class Grab_Rotate90cwFilterCS
+	{
+		const int nCountOfImagesToGrab = 100;
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				using (CStApiAutoInit api = new CStApiAutoInit())
+
+				using (CStSystem system = new CStSystem(eStSystemVendor.Sentech))
+
+				using (CStDevice device = system.CreateFirstStDevice())
+
+				using (CStImageDisplayWnd wnd = new CStImageDisplayWnd())
+
+				using (CStDataStream dataStream = device.CreateStDataStream(0))
+				{
+					Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
+
+                    // ==============================================================================================================
+                    // Demostration of using Filter to rotate image clock wise 90 degree.
+
+                    // Create an ReverseConverter filter object.
+                    CStReverseConverter filter = new CStReverseConverter();
+
+                    // Set ReverseConverter reverse to clock wise 90 degree.
+                    filter.RotationMode = eStRotationMode.Clockwise90;
+
+                    // ==============================================================================================================
+
+					dataStream.StartAcquisition(nCountOfImagesToGrab);
+
+					device.AcquisitionStart();
+
+					while (dataStream.IsGrabbing)
+					{
+						using (CStStreamBuffer streamBuffer = dataStream.RetrieveBuffer(5000))
+						{
+							if (streamBuffer.GetIStStreamBufferInfo().IsImagePresent)
+							{
+								IStImage stImage = streamBuffer.GetIStImage();
+								string strText = device.GetIStDeviceInfo().DisplayName + " ";
+								strText += stImage.ImageWidth + " x " + stImage.ImageHeight + " ";
+								strText += string.Format("{0:F2}[fps]", dataStream.CurrentFPS);
+								wnd.SetUserStatusBarText(strText);
+
+								if (!wnd.IsVisible)
+								{
+									wnd.SetPosition(0, 0, (int)stImage.ImageWidth, (int)stImage.ImageHeight);
+
+									wnd.Show(eStWindowMode.ModalessOnNewThread);
+								}
+
+                                // ==============================================================================================================
+                                // Create another buffer for storing rotated image
+                                CStImageBuffer imageBuffer = CStApiDotNet.CreateStImageBuffer();
+
+                                // Rotate original image and output to another buffer
+                                filter.Convert(stImage, imageBuffer);
+
+                                // Display rotated image
+                                wnd.RegisterIStImage(imageBuffer.GetIStImage());
+                                // ==============================================================================================================
+							}
+							else
+							{
+								Console.WriteLine("Image data does not exist.");
+							}
+						}
+					}
+
+					device.AcquisitionStop();
+
+					dataStream.StopAcquisition();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine("An exception occurred. \r\n" + e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("\r\nPress Enter to exit.");
+				Console.ReadLine();
+			}
+		}
+	}
+}
