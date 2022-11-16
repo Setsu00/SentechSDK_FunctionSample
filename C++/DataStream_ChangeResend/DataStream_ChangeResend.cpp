8c2186e751047f9f4ab95a49a60ee97263419208
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-16 14:48:15 +0900

    2022/11/16 サンプルコードDataStream_ChangeResend追加

---
+++ C++/DataStream_ChangeResend/DataStream_ChangeResend.cpp
@@ -0,0 +1,69 @@
+﻿// DataStream_ChangeResend.cpp : 
+//
+// このサンプルはIStDataStreamのMaximum Resend Requests Per Blockを50(Default)から0に変更する方法を示します。
+//
+
+#include <StApi_TL.h>
+
+using namespace StApi;
+using namespace std;
+const uint64_t nCountOfImagesToGrab = 100;
+
+int main(int /* argc */, char ** /* argv */)
+{
+	try
+	{
+		CStApiAutoInit objStApiAutoInit;
+		CIStSystemPtr pIStSystem(CreateIStSystem(StSystemVendor_Sentech));
+		CIStDevicePtr pIStDevice(pIStSystem->CreateFirstIStDevice());
+		cout << "Device=" << pIStDevice->GetIStDeviceInfo()->GetDisplayName() << endl;
+
+		CIStDataStreamPtr pIStDataStream(pIStDevice->CreateIStDataStream(0));
+
+		// ==============================================================================================================
+		// DataSteam対してのノードマップ生成
+		GenApi::CNodeMapPtr pNodeMapCameraParam(pIStDataStream->GetIStPort()->GetINodeMap());
+
+		// Maximum Resend Requests Per Blockのノードを取得
+		GenApi::CIntegerPtr pIntegerResendRequests(pNodeMapCameraParam->GetNode("MaximumResendRequestsPerBlock"));
+		// Maximum Resend Requests Per Blockに0を設定
+		pIntegerResendRequests->SetValue(0);
+
+		// ==============================================================================================================
+
+		pIStDataStream->StartAcquisition(nCountOfImagesToGrab);
+
+		pIStDevice->AcquisitionStart();
+
+		while (pIStDataStream->IsGrabbing())
+		{
+			CIStStreamBufferPtr pIStStreamBuffer(pIStDataStream->RetrieveBuffer(5000));
+
+			if (pIStStreamBuffer->GetIStStreamBufferInfo()->IsImagePresent())
+			{
+				IStImage *pIStImage = pIStStreamBuffer->GetIStImage();
+
+				cout << "BlockId=" << pIStStreamBuffer->GetIStStreamBufferInfo()->GetFrameID()
+					<< " Size:" << pIStImage->GetImageWidth() << " x " << pIStImage->GetImageHeight()
+					<< " First byte =" << (uint32_t)*(uint8_t*)pIStImage->GetImageBuffer() << endl;
+			}
+			else
+			{
+				cout << "Image data does not exist" << endl;
+			}
+		}
+
+		pIStDevice->AcquisitionStop();
+
+		pIStDataStream->StopAcquisition();
+	}
+	catch (const GenICam::GenericException &e)
+	{
+		cerr << endl << "An exception occurred." << endl << e.GetDescription() << endl;
+	}
+
+	cout << endl << "Press Enter to exit." << endl;
+	while (cin.get() != '\n');
+
+	return(0);
+}
