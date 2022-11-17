607c3c2b0556a05b1496ba5e8a5dd86272de348b
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-17 19:00:01 +0900

    2022/11/17 日本語追加Parameter_SaveToUserSet1

--- C++/Parameter_SaveToUserSet1/Grab_SaveToUserSet1.cpp
+++ C++/Parameter_SaveToUserSet1/Grab_SaveToUserSet1.cpp
@@ -1,4 +1,5 @@
 ﻿/*
+	Grab_SaveToUserSet1.cpp : カメラ設定をUserSet1に保存し、デフォルトに設定するデモ
 	Grab_SaveToUserSet1.cpp : Demostration of saving camera setting to UserSet1 as default.
 */
 
@@ -29,22 +30,28 @@ int main(int /* argc */, char ** /* argv */)
 		CIStDataStreamPtr pIStDataStream(pIStDevice->CreateIStDataStream(0));
 
 		// ==============================================================================================================
+		// 現在の設定をカメラのUserSet1に保存し、電源ON時のデフォルトに設定する
+		// UserSetが保存できるのはカメラが取得中でない場合にのみ使用できます。
 		// Saving current setting to UserSet1 of camera, with setting it as default when camera power on.
 		// Please notice the UserSet saving can be only avaliable when camera is not in acquiring.
 
+		// パラメータにアクセスするためのノードマップポインタを生成
 		// Create NodeMap pointer for accessing parameters
 		GenApi::CNodeMapPtr pNodeMapCameraParam(pIStDevice->GetRemoteIStPort()->GetINodeMap());
 
+		// 設定を保存するUserSet(UserSet1)を選択
 		// Select which UserSet to save the setting (UserSet1)
 		GenApi::CEnumerationPtr pEnumUserSetSelector(pNodeMapCameraParam->GetNode("UserSetSelector"));
 		*pEnumUserSetSelector = "UserSet1";
 
+		// 設定を保存するUserSetを取得し、実行する
 		// Acquire and execute saving setting to UserSet
 		GenApi::CCommandPtr pCmdUsetSetSave(pNodeMapCameraParam->GetNode("UserSetSave"));
 		pCmdUsetSetSave->Execute();
 
 		cout << "Save Current setting to UserSet1 succeed." << endl;
 
+		// 電源ON時にこの設定を使用するためにUserSetDefaultにUserSet1を設定
 		// Set UserSetDefault to UsetSet1 for using this setting when camera power on.
 		GenApi::CEnumerationPtr pIEnumUserSetDefault(pNodeMapCameraParam->GetNode("UserSetDefault"));
 		*pIEnumUserSetDefault = "UserSet1";
