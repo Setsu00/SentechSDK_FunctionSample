f9940c43d1b133d58a29b5e76c20320526ee409e
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-17 18:31:37 +0900

    2022/11/17 日本語追加Interface_UnicastScan

--- C++/Interface_UnicastScan/Interface_UnicastScan.cpp
+++ C++/Interface_UnicastScan/Interface_UnicastScan.cpp
@@ -1,5 +1,6 @@
 ﻿/*
 	Interface_UnicastScan.cpp
+	指定されたIPアドレスを持つユニキャストを使用してデバイスをスキャンするようにGigEインターフェイスを設定する方法をデモします。
 	Demstrate the way of setting GigE interface to scan device with unicast with indicated IP address.
 */
 
@@ -21,17 +22,21 @@ int main(int /* argc */, char ** /* argv */)
 		CStApiAutoInit objStApiAutoInit;
 
 		// =============================================================================================
+		// Note: システムを作成すると、ブロードキャストデバイススキャンが生成されます。
 		// Note: Creating system will  generate a broadcast device scan.
 		CIStSystemPtr pIStSystem(CreateIStSystem(StSystemVendor_Sentech, StInterfaceType_GigEVision));
 
+		// スキャンするためのIPアドレスを入力
 		// Input target IP address for scanning.
 		string strAddress;
 		cout << "Please input camera IP address for unicast camera scanning: " << endl;
 		cin >> strAddress;
 
+		// IPをstringから整数に変換
 		// Convert IP string to integer value
 		const uint32_t nTgtCameraIP = ntohl(inet_addr(strAddress.c_str()));
 
+		// インターフェースの数を取得
 		// Acquire interface counts
 		uint32_t uintInterfaceCnt = pIStSystem->GetInterfaceCount();
 
@@ -39,17 +44,24 @@ int main(int /* argc */, char ** /* argv */)
 		{
 			IStInterface* pInterface = pIStSystem->GetIStInterface(i);
 
+			// 検出コマンドの宛先IPアドレスを設定して、ユニキャストカメラスキャンを有効にします。
+			// *ブロードキャストデバイススキャンのデフォルトは255.255.255.255です。
 			// Set Discovery Command Distination IP Address to enable the unicast camera scanning.
 			// *This is 255.255.255.255 in default for boradcast device scanning.
 			GenApi::CNodeMapPtr pINodeMap(pInterface->GetIStPort()->GetINodeMap());
 			GenApi::CIntegerPtr pInteger(pINodeMap->GetNode("GevDeviceDiscoveryCommandDestinationIPAddress"));
 			pInteger->SetValue(nTgtCameraIP);
 
+			// 接続先IPの設定後、実際のスキャンプロセスの更新を呼び出します。
+			// ユニキャストスキャンパッケージは、この機能を介して送信されます。
 			// After setting distination IP, call update for actual scanning process.
 			// Unicast scanning package will be sent via this function.
 			pInterface->UpdateDeviceList();
 		}
 		
+		// 指定されたIPアドレスを持つカメラとの接続を試みる
+		// もし見つかれば、デバイス名を表示
+		// 見つからなければ、例外処理を行う
 		// Try to connect to any camera with indicated IP address.
 		// If found, show the device display name;
 		// if not found it will goes to exception handling.
