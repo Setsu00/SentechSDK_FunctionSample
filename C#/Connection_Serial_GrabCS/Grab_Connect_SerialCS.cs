e129d9fc8d36ca59406c875e9d0e2b24d862df79
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 17:59:29 +0900

    2022/11/30 日本語追加Connection_Serial_GrabCS

--- C#/Connection_Serial_GrabCS/Grab_Connect_SerialCS.cs
+++ C#/Connection_Serial_GrabCS/Grab_Connect_SerialCS.cs
@@ -17,23 +17,30 @@ namespace Grab
 
                 using (CStSystem system = new CStSystem(eStSystemVendor.Sentech))
                 {
+                    // ==== カメラシリアルでターゲットカメラに接続 ================================================================
                     // ==== Connect to target camera with camera serial. ================================================================
+                    // シリアル入力
                     // Input Serial.
                     System.Console.WriteLine("Please input serial number of target camera: ");
                     string sTgtCameraSerial = System.Console.ReadLine();
 
+                    // 入力をクリア
                     // Acquire interface counts.
                     uint uiInterfaceCnt = system.InterfaceCount;
 
+                    // インターフェースの数を取得
                     // Prepear gcstring for saving device ID if hit.
                     string strTgtDeviceID = "";
 
+                    // ヒットした場合、デバイスIDを保存するためのgcstringを準備
                     // Prepear for # of interface for later use.
                     uint uiTgtInterfaceNo = 0;
 
+                    // ヒットフラグ
                     // Camera found hit flag.
                     bool bHit = false;
 
+                    // ターゲットカメラが存在するか全てのインターフェースをチェック
                     // Check all interface if target camera is exist.
                     for (uint i = 0; i < uiInterfaceCnt; i++ )
                     {
@@ -56,6 +63,7 @@ namespace Grab
 
                     if(!bHit)
                     {
+                        // 見つからなければメッセージと共に終了
                         // Not found, exit program with message.
                         System.Console.WriteLine("Target camera not found.");
                         System.Console.WriteLine("Press Enter to exit.");
@@ -63,6 +71,7 @@ namespace Grab
                         return;
                     }
 
+                        // 見つけたデバイスIDを使うためIStDevice経由で生成
                         // Create IStDevice via using found device ID.
                         using (CStDevice device = system.GetIStInterface(uiTgtInterfaceNo).CreateStDevice(strTgtDeviceID))
 
