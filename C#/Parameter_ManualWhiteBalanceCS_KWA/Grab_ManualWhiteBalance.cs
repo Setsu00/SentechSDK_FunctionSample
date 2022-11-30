e2fb3fc4db44f93a05cebdf87ae76519f5bc155a
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 18:25:32 +0900

    2022/11/30 日本語追加Parameter_ManualWhiteBalanceCS_KWA

--- C#/Parameter_ManualWhiteBalanceCS_KWA/Grab_ManualWhiteBalance.cs
+++ C#/Parameter_ManualWhiteBalanceCS_KWA/Grab_ManualWhiteBalance.cs
@@ -1,4 +1,5 @@
 ﻿/*
+    Manual White Balance: 手動で全色のホワイトバランス比率を10に設定する
 	Manual White Balance: Manually set white balance ratio of all color to 10.
 */
 
@@ -31,33 +32,41 @@ namespace Grab
 					Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
 
                     // ==============================================================================================================
-                    // Demostration of Setting Hardware Trigger ON with active high
+                    // ホワイトバランスを手動に変更し、全てのバランス比率を10に設定するデモ
+                    // Demostration of changing white balance to manual with setting all balance ratio to 10
 
+                    // パラメータにアクセスするためのノードマップポインタを生成
                     // Create NodeMap pointer for accessing parameters
                     INodeMap nodeMap = device.GetRemoteIStPort().GetINodeMap();
 
+                    // 入力値を手動にするためBalanceWhiteAutoをPreset0に切替
                     // Switch BalanceWhiteAuto to Preset0 for manual input value
                     IEnum enumBalanceWhiteAuto = nodeMap.GetNode<IEnum>("BalanceWhiteAuto");
                     enumBalanceWhiteAuto.FromString("Preset0");
 
+                    // 赤の比率値にアクセスするためBalanceRatioSelectorをRedに切替
                     // Switch balance ration selector to Red for access Red ratio value
                     IEnum enumBalanceRatioSelector = nodeMap.GetNode<IEnum>("BalanceRatioSelector");
                     enumBalanceRatioSelector.FromString("Red");
 
+                    // BalanceRatioのノードを取得
                     // Get Node for BalanceRatio
                     IFloat floatBalanceRatio = nodeMap.GetNode<IFloat>("BalanceRatio");
+                    // バランス比率に10を設定
                     // Set BalanceRatio to 10
                     floatBalanceRatio.Value = 10;
 
+                    // 青の比率値にアクセスするためBalanceRatioSelectorをBlueに切替
                     // Switch balance ration selector to Blue for access Blue ratio value
                     enumBalanceRatioSelector.FromString("Blue");
-
+                    // バランス比率に10を設定
                     // Set BalanceRatio to 10
                     floatBalanceRatio.Value = 10;
 
+                    // 緑の比率値にアクセスするためBalanceRatioSelectorをGreenに切替
                     // Switch balance ration selector to Green for access Green ratio value
                     enumBalanceRatioSelector.FromString("Green");
-
+                    // バランス比率に10を設定
                     // Set BalanceRatio to 10
                     floatBalanceRatio.Value = 10;
 
