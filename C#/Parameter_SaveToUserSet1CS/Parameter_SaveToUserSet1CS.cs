e4b8ab18766ddc43d4292f8578cf0f2f4f25b77f
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 18:32:04 +0900

    2022/11/30 日本語追加Parameter_SaveToUserSet1CS

--- C#/Parameter_SaveToUserSet1CS/Parameter_SaveToUserSet1CS.cs
+++ C#/Parameter_SaveToUserSet1CS/Parameter_SaveToUserSet1CS.cs
@@ -1,4 +1,5 @@
 ﻿/*
+    Parameter_SaveToUserSet1CS.cs : カメラ設定をUserSet1に保存し、デフォルトに設定するデモ
 	Parameter_SaveToUserSet1CS.cs : Demostration of saving camera setting to UserSet1 as default.
 	
 */
@@ -32,24 +33,30 @@ namespace Grab
 					Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
 
                     // ==============================================================================================================
+                    // 現在の設定をカメラのUserSet1に保存し、電源ON時のデフォルトに設定する
+                    // UserSetが保存できるのはカメラが取得中でない場合にのみ使用できます。
                     // Saving current setting to UserSet1 of camera, with setting it as default when camera power on.
-					// Please notice the UserSet saving can be only avaliable when camera is not in acquiring.
+                    // Please notice the UserSet saving can be only avaliable when camera is not in acquiring.
 
+                    // パラメータにアクセスするためのノードマップポインタを生成
                     // Create NodeMap pointer for accessing parameters
                     INodeMap nodeMap = device.GetRemoteIStPort().GetINodeMap();
 
+                    // 設定を保存するUserSet(UserSet1)を選択
                     // Select which UserSet to save the setting (UserSet1)
                     IEnum enumUserSetSelector = nodeMap.GetNode<IEnum>("UserSetSelector");
 					enumUserSetSelector.FromString("UserSet1");
 
-					// Acquire and execute saving setting to UserSet
-					ICommand cmdSaveToUserSet = nodeMap.GetNode<ICommand>("UserSetSave");
+                    // 設定を保存するUserSetを取得し、実行する
+                    // Acquire and execute saving setting to UserSet
+                    ICommand cmdSaveToUserSet = nodeMap.GetNode<ICommand>("UserSetSave");
 					cmdSaveToUserSet.Execute();
 
 					Console.WriteLine("Save Current setting to UserSet1 succeed.");
 
-					// Set UserSetDefault to UsetSet1 for using this setting when camera power on.
-					IEnum enumUserSetDefault = nodeMap.GetNode<IEnum>("UserSetDefault");
+                    // 電源ON時にこの設定を使用するためにUserSetDefaultにUserSet1を設定
+                    // Set UserSetDefault to UsetSet1 for using this setting when camera power on.
+                    IEnum enumUserSetDefault = nodeMap.GetNode<IEnum>("UserSetDefault");
 					enumUserSetDefault.FromString("UserSet1");
 
 					Console.WriteLine("Set UserSetDefault to UserSet1 succeed.");
