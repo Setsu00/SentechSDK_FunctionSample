98c0a668d33f9a01409cf1df5567f152af5ed7fd
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 18:17:06 +0900

    2022/11/30 日本語追加Interface_UnicastScanCS

--- C#/Interface_UnicastScanCS/Interface_UnicastScanCS.cs
+++ C#/Interface_UnicastScanCS/Interface_UnicastScanCS.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+	Interface_UnicastScanCS.cs
+	指定されたIPアドレスを持つユニキャストを使用してデバイスをスキャンするようにGigEインターフェイスを設定する方法をデモします。
+	Demstrate the way of setting GigE interface to scan device with unicast with indicated IP address.
+*/
+
+using System;
 using System.Net;
 using System.Threading;
 using Sentech.GenApiDotNET;
@@ -16,18 +22,21 @@ namespace Grab
 			{
 				using (CStApiAutoInit api = new CStApiAutoInit())
 
-				// =============================================================================================
-				// Note: Creating system will  generate a broadcast device scan.
-				using (CStSystem system = new CStSystem(eStSystemVendor.Sentech, eStInterfaceType.GigEVision))
+                // =============================================================================================
+                // Note: システムを作成すると、ブロードキャストデバイススキャンが生成されます。
+                // Note: Creating system will  generate a broadcast device scan.
+                using (CStSystem system = new CStSystem(eStSystemVendor.Sentech, eStInterfaceType.GigEVision))
 				{
-					// Input target IP address for scanning.
-					Console.Write("Please input camera IP address for unicast camera scanning: ");
+                    // スキャンするためのIPアドレスを入力
+                    // Input target IP address for scanning.
+                    Console.Write("Please input camera IP address for unicast camera scanning: ");
 					string value = Console.ReadLine();
 
 					IPAddress ipaddr;
 
-					// Try parse the input address, if failed exit program.
-					if (!IPAddress.TryParse(value.Trim(), out ipaddr))
+                    // 終了プログラムに失敗した場合は、入力アドレスを解析してください。
+                    // Try parse the input address, if failed exit program.
+                    if (!IPAddress.TryParse(value.Trim(), out ipaddr))
 					{
 						Console.WriteLine("IP address is not valid.");
 						Console.WriteLine("\r\nPress Enter to exit.");
@@ -35,32 +44,40 @@ namespace Grab
 						Environment.Exit(0);
 					}
 
-					// Convert the IP address string to a 32-bit number.
-					byte[] bytes = ipaddr.GetAddressBytes();
+                    // IPアドレス文字列を32ビットの数値に変換します。
+                    // Convert the IP address string to a 32-bit number.
+                    byte[] bytes = ipaddr.GetAddressBytes();
 					uint uiTgtDevIPAddress = (uint)(IPAddress.NetworkToHostOrder(BitConverter.ToUInt32(bytes, 0)) >> 32);
 
-					// Acquire interface counts
-					uint uiCntInterface = system.InterfaceCount;
+                    // インターフェースの数を取得
+                    // Acquire interface counts
+                    uint uiCntInterface = system.InterfaceCount;
 
 					for (uint i = 0; i < uiCntInterface; i++)
 					{
 						IStInterface pInterface = system.GetIStInterface(i);
-						// Set Discovery Command Distination IP Address to enable the unicast camera scanning.
-						// *This is 255.255.255.255 in default for boradcast device scanning.
-						INodeMap nodeMapInterface = pInterface.GetIStPort().GetINodeMap();
+                        // 検出コマンドの宛先IPアドレスを設定して、ユニキャストカメラスキャンを有効にします。
+                        // *ブロードキャストデバイススキャンのデフォルトは255.255.255.255です。
+                        // Set Discovery Command Distination IP Address to enable the unicast camera scanning.
+                        // *This is 255.255.255.255 in default for boradcast device scanning.
+                        INodeMap nodeMapInterface = pInterface.GetIStPort().GetINodeMap();
 						IInteger intDistiantionIPAddress = nodeMapInterface.GetNode<IInteger>("GevDeviceDiscoveryCommandDestinationIPAddress");
 						intDistiantionIPAddress.SetValue(uiTgtDevIPAddress, false);
 
-						// After setting distination IP, call update for actual scanning process.
-						// Unicast scanning package will be sent via this function.
-						pInterface.UpdateDeviceList();
+                        // 接続先IPの設定後、実際のスキャンプロセスの更新を呼び出します。
+                        // ユニキャストスキャンパッケージは、この機能を介して送信されます。
+                        // After setting distination IP, call update for actual scanning process.
+                        // Unicast scanning package will be sent via this function.
+                        pInterface.UpdateDeviceList();
 
 					}
 					using (CStDevice device = system.CreateFirstStDevice())
 					{
-						// Print out information of device if found and connected.
-						// Otherwise it will goes into exception handling.
-						Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
+                        // デバイスが見つかり、接続されている場合は、デバイスの情報を出力します。
+                        // それ以外の場合は例外処理に入ります。
+                        // Print out information of device if found and connected.
+                        // Otherwise it will goes into exception handling.
+                        Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
 						Console.WriteLine("Device found and connected.");
 					}
 				}
