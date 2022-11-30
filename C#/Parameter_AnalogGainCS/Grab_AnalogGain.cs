7abd042003d60f0846e4a54cb4e4d28bcb55724f
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 18:19:05 +0900

    2022/11/30 日本語追加Parameter_AnalogGainCS

--- C#/Parameter_AnalogGainCS/Grab_AnalogGain.cs
+++ C#/Parameter_AnalogGainCS/Grab_AnalogGain.cs
@@ -1,4 +1,5 @@
 ﻿/*
+    Analog Gain Setting: アナログゲインを2dBに設定する。
 	Analog Gain Setting: Set analog gain to 2 dB.
 */
 
@@ -31,22 +32,28 @@ namespace Grab
 					Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
 
                     // ==============================================================================================================
+                    // アナログゲインを2dBに設定するデモ
                     // Demostration of set analog Gain to 2dB.
 
+                    // パラメータにアクセスするためのノードマップポインタを生成
                     // Create NodeMap pointer for accessing parameters
                     INodeMap nodeMap = device.GetRemoteIStPort().GetINodeMap();
 
+                    // GainAutoをOffに切替。GainAutoがOnの場合、ゲイン値を設定できません。
                     // Switch off gain auto. If gain auto is on, gain value cannot be set.
                     IEnum enumGainAuto = nodeMap.GetNode<IEnum>("GainAuto");
                     enumGainAuto.FromString("Off");
 
+                    // アナログゲインを設定するため、アナログゲインにアクセスするためにGainSelectorをAnalogAllに設定する必要がある
                     // For setting analog gain, gain selector need to be set to AnalogAll to access analog gain.
                     IEnum enumGainSelector = nodeMap.GetNode<IEnum>("GainSelector");
                     enumGainSelector.FromString("AnalogAll");
 
+                    // Gainノードを取得
                     // Get Node for Gain
                     IFloat floatGain = nodeMap.GetNode<IFloat>("Gain");
-		            // Set Gain to 2 dB.
+                    // ゲインに2dBを設定
+                    // Set Gain to 2 dB.
                     floatGain.Value = 20;
 
                     // ==============================================================================================================
