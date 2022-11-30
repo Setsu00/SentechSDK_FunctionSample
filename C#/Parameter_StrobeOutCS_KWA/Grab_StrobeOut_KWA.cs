149152757d6ecbb902420a9d26c2bea112b408bd
naoki-kamisaki <naoki-kamisaki@omron.com>
2022-11-30 18:33:39 +0900

    2022/11/30 日本語追加Parameter_StrobeOutCS_KWA

--- C#/Parameter_StrobeOutCS_KWA/Grab_StrobeOut_KWA.cs
+++ C#/Parameter_StrobeOutCS_KWA/Grab_StrobeOut_KWA.cs
@@ -1,6 +1,7 @@
 ﻿/*
+    Strobe Out: Line 2を出力1000 us信号のStrobe Outに設定します。(カメラがExposure Activeのときに1000 us信号を出力する。)
 	Strobe Out: Set Line2 as Strobe Out with output 1000us signal. (Output 1000us signal when camera exposure active.) 
-    * For STC-MB/MCS Series only
+    * For STC-MB/MCS Series only(STC-MB/MCSシリーズのみ対応)
 */
 
 #define ENABLED_ST_GUI
@@ -32,29 +33,36 @@ namespace Grab
 					Console.WriteLine("Device=" + device.GetIStDeviceInfo().DisplayName);
 
                     // ==============================================================================================================
+                    // Line2にStrobe Outを設定するデモ
                     // Demostration of Setting Line2 as Strobe Out
 
+                    // パラメータにアクセスするためのノードマップポインタを生成
                     // Create NodeMap pointer for accessing parameters
                     INodeMap nodeMap = device.GetRemoteIStPort().GetINodeMap();
 
+                    // Line2に出力を設定
                     // Set Line2 to output
                     IEnum enumLineSelector = nodeMap.GetNode<IEnum>("LineSelector");
                     enumLineSelector.FromString("Line2");
                     IEnum enumLineMode = nodeMap.GetNode<IEnum>("LineMode");
                     enumLineMode.FromString("Output");
 
+                    // Line2出力をTimer 0 Activeに切替
                     // Switch Line2 output source to Timer 0 Active
                     IEnum enumLineSource = nodeMap.GetNode<IEnum>("LineSource");
                     enumLineSource.FromString("Timer0Active");
 
+                    // Timer0のTriggerSourceをExposure Startに設定すると、カメラの露光開始時にTimer0が信号を出力
                     // Set Timer 0 trigger source to Exposure Start, which means Timer 0 will output signal when camera start exposure
                     IEnum enumTimerSelector = nodeMap.GetNode<IEnum>("TimerSelector");
                     enumTimerSelector.FromString("Timer0");
 
+                    // Timer0のTriggerSourceとしてExposureStartを設定
                     // Set exposure start as the trigger source of Timer0
                     IEnum enumTimerTriggerSource = nodeMap.GetNode<IEnum>("TimerTriggerSource");
                     enumTimerTriggerSource.FromString("ExposureStart");
 
+                    // Timer0出力期間に1000usを設定
                     // Set Timer0 output duration to 1000us
                     IFloat floatTimerDuration = nodeMap.GetNode<IFloat>("TimerDuration");
                     floatTimerDuration.Value = 1000;
